a283d1354b5d58d7e52b7c6c99b8ac9f77df3ee5
Unkreativ <Unkreativ@Home>
2025-07-30 15:23:27 +0200

    "Requiered Field"

--- PullCalc/Program.cs
+++ PullCalc/Program.cs
@@ -3,17 +3,7 @@ using PullCalc.Banner;
 using PullCalc.Event;
 
 ABanner[] banners =
-[
-    //new LimitedBanner("Pepe", new DateTime(2025, 1, 5), null, null, new LargeEvent()),
-
-    //new StandardBanner("Pink girl", new DateTime(2025, 2, 5), null, 90, new SideEvent()),
-    //new CollabBanner("Food", new DateTime(2025, 3, 7), null, -1, new StandardEvent()),
-    //new StandardBanner("Siege", new DateTime(2025, 4, 6), null, null, new SideEvent()),
-
-    //new LimitedBanner("laptop", new DateTime(2025, 4, 24), null, -1, new LargeEvent()),
-    //new StandardBanner("thorny", new DateTime(2025, 6, 5), null, null, new StandardEvent()),
-    new LimitedBanner("blazing sun", new DateTime(2025, 7, 5), null, null, new LargeEvent()),
-    
+[  
     new StandardBanner("red'n'black", new DateTime(2025, 8, 5), null, null, new StandardEvent()),
     new StandardBanner("anti reed", new DateTime(2025, 9, 5), null, null, new StandardEvent()),
     new StandardBanner("energy drink", new DateTime(2025, 10, 5), null, null, new NewChapter()),
